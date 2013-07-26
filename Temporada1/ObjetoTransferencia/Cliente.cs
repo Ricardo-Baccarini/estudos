ff7e1360af32f707fb4a8a6294b494ae366e94f9
Ricardo Baccarini <ricardo.baccarini@gmail.com>
2013-07-26 18:10:26 -0300

    Inicio

---
+++ Temporada1/ObjetoTransferencia/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ObjetoTransferencia
+{
+    public class Cliente
+    {
+        // Variaveis
+        public int idCliente { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Boolean Sexo { get; set; }
+        public decimal LimiteCompra { get; set; }
+    }
+}
