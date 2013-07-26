e3fda8be3f65fb8f13ee51aea6a75977d3f76ef5
Ricardo Baccarini <ricardo.baccarini@gmail.com>
2013-07-26 18:13:29 -0300

    Inicio

---
+++ Temporada1/ObjetoTransferencia/ClienteColecao.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ObjetoTransferencia
+{
+    public class ClienteColecao : List< Cliente > // Herança Utiliza-se ':' "ClienteColecao herda Cliente
+    {
+
+    }
+}
