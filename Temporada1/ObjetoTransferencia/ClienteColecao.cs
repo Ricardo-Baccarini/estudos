01484c1b1dc4ee0c2412a38d7abb711f98ac6395
Ricardo Baccarini <ricardo.baccarini@gmail.com>
2013-07-26 18:11:49 -0300

    Revert "Inicio"
    This reverts commit ff7e1360af32f707fb4a8a6294b494ae366e94f9.

--- Temporada1/ObjetoTransferencia/ClienteColecao.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ObjetoTransferencia
-{
-    public class ClienteColecao : List< Cliente > // Herança Utiliza-se ':' "ClienteColecao herda Cliente
-    {
-
-    }
-}
